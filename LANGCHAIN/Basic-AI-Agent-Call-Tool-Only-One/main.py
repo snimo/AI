7c62a9667f0432e7006a965268eac22df281accb
snimo <sebastian@zenithtek.io>
2025-09-21 15:54:19 -0300

    All

--- LANGCHAIN/Basic-AI-Agent-Call-Tool-Only-One/main.py
+++ LANGCHAIN/Basic-AI-Agent-Call-Tool-Only-One/main.py
@@ -12,7 +12,6 @@ checkpointer = InMemorySaver()
 llm = init_chat_model("llama3-8b-8192", model_provider="groq", temperature=0.1)
 
 
-
 @tool
 def create_gmail_draft(to: List[str], subject: str, message: str) -> str:
     """Crea un borrador de correo en Gmail.
@@ -44,64 +43,68 @@ def create_gmail_draft(to: List[str], subject: str, message: str) -> str:
 
 
 agent = create_react_agent(
-    model = llm, # Choice of the LLM
-    tools = [create_gmail_draft] , # Tools we want our LLM to have
-    name = "email_agent", # Name of our agent
-    prompt = """
+    model=llm,  # Choice of the LLM
+    tools=[create_gmail_draft],  # Tools we want our LLM to have
+    name="email_agent",  # Name of our agent
+    prompt="""
         Las tools disponibles son create_gmail_draft. 
         La respuesta final deberia ser algo ai:
         
         Se genero exitosame el correo draft dirigo al siguiente correo: snimo@mas.com.ar 
 
-    """, # System Prompt
+    """,  # System Prompt
 )
+
 
 # 4. Definir el estado del agente
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
-    quantity : 0
+    quantity: 0
 
 
 # Definimos un nuevo nodo
 def update_quantity(state: AgentState) -> AgentState:
     state["quantity"] = state.get("quantity", 0) + 1
-    
+
     return state
+
 
 # 5. Crear el grafo
 graph = StateGraph(AgentState)
-graph.add_node("upd_quantity" , update_quantity)
+graph.add_node("upd_quantity", update_quantity)
 graph.add_node("agent", agent)
 
 graph.set_entry_point("agent")
-graph.add_edge("agent","upd_quantity")
+graph.add_edge("agent", "upd_quantity")
 graph.set_finish_point("upd_quantity")
 
 # 6. Compilar el grafo
 email_app = graph.compile(checkpointer=checkpointer)
 
 result = email_app.invoke({
-    "messages": [HumanMessage(content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
-     config={"configurable": {"thread_id": "user_1"},
-})
+    "messages": [HumanMessage(
+        content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
+    config={"configurable": {"thread_id": "user_1"},
+            })
 
 result = email_app.invoke(
-    {"messages": [HumanMessage(content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
-     config={"configurable": {"thread_id": "user_1"},
-})
+    {"messages": [HumanMessage(
+        content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
+    config={"configurable": {"thread_id": "user_1"},
+            })
 
 for msg in result["messages"]:
-    print(type(msg) , msg)
+    print(type(msg), msg)
 
-print("La cantidad es ",result.get("quantity"))
+print("La cantidad es ", result.get("quantity"))
 
 result = email_app.invoke({
-    "messages": [HumanMessage(content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
-     config={"configurable": {"thread_id": "user_2"},
-})
+    "messages": [HumanMessage(
+        content="Quiero crear un borrador de correo para LUCAS@ejemplo.com con asunto 'Cobranza' y el cuerpo que diga 'Toda la deuda'")]},
+    config={"configurable": {"thread_id": "user_2"},
+            })
 
 for msg in result["messages"]:
-    print(type(msg) , msg)
+    print(type(msg), msg)
 
-print("La cantidad es ",result.get("quantity"))
-
+print("La cantidad es ", result.get("quantity"))
